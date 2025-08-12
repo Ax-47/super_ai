1221aa474c81f05800c8d6e1a16a142336c804e0
Ax-47 <armingLwnza@gmail.com>
2025-08-12 22:26:54 +0700

    fix: error handle

--- src/app/api/[[...slugs]]/usecases/prompt.ts
+++ src/app/api/[[...slugs]]/usecases/prompt.ts
@@ -15,16 +15,19 @@ export class ChatUsecase {
     for await (const chunk of this.chat_repo.prompt(classifierPrompt))
       res += chunk;
     const predictedCategory = res.trim().replace("-", "").trim();
-
-    const { metadatas } = await this.chat_repo.search(predictedCategory, prompt);
-    if (!metadatas || !metadatas[0] || !metadatas[0].answer) {
+    try {
+      const { metadatas } = await this.chat_repo.search(predictedCategory, prompt);
+      if (!metadatas || !metadatas[0] || !metadatas[0].answer) {
+        yield "ขออภัย ฉันไม่สามารถหาข้อมูลที่ตรงกับคำถามของคุณได้ในขณะนี้";
+        return;
+      }
+      const retrievedAnswer = (metadatas[0]).answer;
+      const generatorPrompt = this.chat_repo.buildGeneratorPrompt(prompt, retrievedAnswer.toString());
+      for await (const chunk of this.chat_repo.prompt(generatorPrompt))
+        yield chunk;
+    } catch {
       yield "ขออภัย ฉันไม่สามารถหาข้อมูลที่ตรงกับคำถามของคุณได้ในขณะนี้";
       return;
     }
-    const retrievedAnswer = (metadatas[0]).answer;
-    const generatorPrompt = this.chat_repo.buildGeneratorPrompt(prompt, retrievedAnswer.toString());
-    for await (const chunk of this.chat_repo.prompt(generatorPrompt))
-      yield chunk;
-
   }
 }
